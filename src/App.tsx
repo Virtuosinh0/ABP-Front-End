9769f04e604f77bc2db5cc460ec375b59423915c
Silvio Virtuoso Júnior <141785530+Virtuosinh0@users.noreply.github.com>
2023-11-30 22:54:42 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginForm from './LoginForm';
-import Inquilino from './Inquilino';
 import Locador from './Locador';
+import Inquilinos from './Inquilinos';
+import Imoveis from './Imoveis';
+import ContasReceber from './ContasReceber';
+import RootLayout from './RootLayout';
+import LoginForm from './LoginForm'; // Presume-se que seja a página inicial de login
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/inquilino" element={<Inquilino />} />
-        <Route path="/locador" element={<Locador />} />
+        {/* Rota para a página de login */}
         <Route path="/" element={<LoginForm />} />
+        
+        {/* Rota para a página do Locador com layout específico */}
+        <Route path="/locador/*" element={<RootLayout><Locador /></RootLayout>} />
+
+        {/* Rota para a página de Inquilinos com layout específico */}
+        <Route path="/inquilinos/*" element={<RootLayout><Inquilinos /></RootLayout>} />
+        
+        {/* Rota para a página de Imóveis com layout específico */}
+        <Route path="/imoveis/*" element={<RootLayout><Imoveis /></RootLayout>} />
+        
+        {/* Rota para a página de Contas a Receber com layout específico */}
+        <Route path="/contas-receber/*" element={<RootLayout><ContasReceber /></RootLayout>} />
       </Routes>
     </Router>
   );
