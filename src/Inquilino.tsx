b0e9fc39eaa3214cac246eaeb2126c5a01704030
Silvio Virtuoso Júnior <141785530+Virtuosinh0@users.noreply.github.com>
2023-11-30 22:55:49 -0300

    Update Inquilino.tsx

---
+++ src/Inquilino.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Inquilino: React.FC = () => {
+  // Aqui você pode incluir a lógica necessária para obter os dados do inquilino, exibição de informações, etc.
+
+  return (
+    <div className="container">
+      <h2>Bem-vindo, Inquilino!</h2>
+      {/* Adicione aqui os componentes e informações que deseja exibir para o inquilino */}
+    </div>
+  );
+};
+
+export default Inquilino;
