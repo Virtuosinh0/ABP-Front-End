91198833d78dead39b541b90bec68adbcfb1a185
Kauan Nunes Aguiar <133172749+KauanNunesAguiar@users.noreply.github.com>
2023-12-03 18:31:52 -0300

    Update App.tsx
    * Comprimido a parte de links
    * Agora o nome de login é enviado para as telas de inquilino e locador

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Locador from './Locador';
 import Inquilinos from './Inquilinos';
@@ -8,54 +8,24 @@ import RootLayout from './RootLayout';
 import LoginForm from './LoginForm';
 
 const App: React.FC = () => {
+  const [user, setUser] = useState<string>(''); 
+
+  const setLoggedInUser = (username: string) => {
+    setUser(username);
+  };
+
   return (
     <Router>
       <Routes>
-        {/* Rota para a página de login */}
-        <Route path="/" element={<LoginForm />} />
-
-        {/* Rota para a página do Locador com layout específico */}
-        <Route
-          path="/Locador/*"
-          element={
-            <RootLayout>
-              <Locador />
-            </RootLayout>
-          }
-        />
-
-        {/* Rota para a página de Inquilinos com layout específico */}
-        <Route
-          path="/Inquilinos/*"
-          element={
-            <RootLayout>
-              <Inquilinos />
-            </RootLayout>
-          }
-        />
-
-        {/* Rota para a página de Imóveis com layout específico */}
-        <Route
-          path="/Imoveis/*"
-          element={
-            <RootLayout>
-              <Imoveis />
-            </RootLayout>
-          }
-        />
-
-        {/* Rota para a página de Contas a Receber com layout específico */}
-        <Route
-          path="/Contas-Receber/*"
-          element={
-            <RootLayout>
-              <ContasReceber />
-            </RootLayout>
-          }
-        />
+        <Route path="/" element={<LoginForm setLoggedInUser={setLoggedInUser} />} />
+        <Route path="/locador/*" element={<RootLayout><Locador user={user} /></RootLayout>} />
+        <Route path="/inquilinos/*" element={<RootLayout><Inquilinos user={user} /></RootLayout>} />
+        <Route path="/imoveis/*" element={<RootLayout><Imoveis user={user} /></RootLayout>} />
+        <Route path="/contas-receber/*" element={<RootLayout><ContasReceber user={user} /></RootLayout>} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
