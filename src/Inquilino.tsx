66ce415dc6477fc5b49ee9d9b7ff7aa555dbdb21
Kauan Nunes Aguiar <133172749+KauanNunesAguiar@users.noreply.github.com>
2023-12-03 18:34:53 -0300

    Update Inquilino.tsx
    Ajuste para as mudanças do App.tsx

--- src/Inquilino.tsx
+++ src/Inquilino.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Inquilino: React.FC = () => {
-  // Aqui você pode incluir a lógica necessária para obter os dados do inquilino, exibição de informações, etc.
+interface InquilinoProps {
+  user: string;
+}
 
+const Inquilino: React.FC<InquilinoProps> = ({ user }) => {
   return (
     <div className="container">
-      <h2>Bem-vindo, Inquilino!</h2>
+      <h2>{`Bem-vindo, ${user}!`}</h2>
       {/* Adicione aqui os componentes e informações que deseja exibir para o inquilino */}
     </div>
   );
